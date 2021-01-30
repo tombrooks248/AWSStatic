f25c57dfacc16de816d613523b542cce87966739
Tom Brooks <tombrooks248@gmail.com>
2021-01-30 13:40:15 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          THIS is TOM's website now !!!!! MuHahahahaha
         </a>
       </header>
     </div>
